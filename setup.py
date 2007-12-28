3d93a7dae91c89747819cea3f034c2577f997598
hongqn <hongqn@bc624eda-5042-0410-a9c9-c735abdd7021>
2007-12-28 14:10:22 +0000

    add montypython example

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
         name = 'douban-python',
-        version = '0.1',
+        version = '0.1.1',
         description = "Python client library for Douban APIs",
         license = "Apache 2.0",
-        url = "http://www.douban.com/service/api/",
-        install_requires = ['setuptools', 'gdata'],
-        tests_requires = ['nose'],
+        author = "Qiangning Hong",
+        author_email = "hongqn@gmail.com",
+        url = "http://www.douban.com/service/apidoc/",
+        install_requires = ['setuptools', 'gdata.py'],
+        tests_require = ['nose'],
         packages = find_packages(),
         test_suite = 'nose.collector',
 )
