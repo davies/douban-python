bcfb3e6f744c4e21ab214e703a1322efd5b9af7f
hongqn <hongqn@bc624eda-5042-0410-a9c9-c735abdd7021>
2007-12-28 08:00:51 +0000

    add more search interface

--- douban/service.py
+++ douban/service.py
@@ -2,6 +2,7 @@
 
 import gdata.service
 import douban
+import urllib
 
 class DoubanService(gdata.service.GDataService):
     def __init__(self, email=None, password=None, source=None,
@@ -17,11 +18,26 @@ class DoubanService(gdata.service.GDataService):
     def GetPeopleFeed(self, uri):
         return self.Get(uri, converter=douban.PeopleFeedFromString)
 
+    def SearchPeople(self, text_query, start_index=None, max_results=None):
+        query = Query('/people', text_query, start_index=start_index,
+                max_results=max_results)
+        return self.GetPeopleFeed(query.ToUri())
+
     def GetBook(self, uri):
         return self.Get(uri, converter=douban.BookEntryFromString)
 
     def GetBookFeed(self, uri):
         return self.Get(uri, converter=douban.BookFeedFromString)
+
+    def SearchBook(self, text_query, start_index=None, max_results=None):
+        query = Query('/book/subjects', text_query=text_query,
+                start_index=start_index, max_results=max_results)
+        return self.GetBookFeed(query.ToUri())
+
+    def QueryBookByTag(self, tag, start_index=None, max_results=None):
+        query = Query('/book/subjects', text_query=None,
+                start_index=start_index, max_results=max_results, tag=tag)
+        return self.GetBookFeed(query.ToUri())
 
     def GetMovie(self, uri):
         return self.Get(uri, converter=douban.MovieEntryFromString)
@@ -29,11 +45,31 @@ class DoubanService(gdata.service.GDataService):
     def GetMovieFeed(self, uri):
         return self.Get(uri, converter=douban.MovieFeedFromString)
 
+    def SearchMovie(self, text_query, start_index=None, max_results=None):
+        query = Query('/movie/subjects', text_query=text_query,
+                start_index=start_index, max_results=max_results)
+        return self.GetMovieFeed(query.ToUri())
+
+    def QueryMovieByTag(self, tag, start_index=None, max_results=None):
+        query = Query('/movie/subjects', text_query=None,
+                start_index=start_index, max_results=max_results, tag=tag)
+        return self.GetMovieFeed(query.ToUri())
+
     def GetMusic(self, uri):
         return self.Get(uri, converter=douban.MusicEntryFromString)
 
     def GetMusicFeed(self, uri):
         return self.Get(uri, converter=douban.MusicFeedFromString)
+
+    def SearchMusic(self, text_query, start_index=None, max_results=None):
+        query = Query('/music/subjects', text_query=text_query,
+                start_index=start_index, max_results=max_results)
+        return self.GetMusicFeed(query.ToUri())
+
+    def QueryMusicByTag(self, tag, start_index=None, max_results=None):
+        query = Query('/music/subjects', text_query=None,
+                start_index=start_index, max_results=max_results, tag=tag)
+        return self.GetMusicFeed(query.ToUri())
 
     def GetReview(self, uri):
         return self.Get(uri, converter=douban.ReviewEntryFromString)
@@ -44,5 +80,25 @@ class DoubanService(gdata.service.GDataService):
     def GetCollectionFeed(self, uri):
         return self.Get(uri, converter=douban.CollectionFeedFromString)
 
+    def Get(self, uri, converter=None):
+        if self.api_key:
+            param = urllib.urlencode([('apikey', self.api_key)])
+            if '?' in uri:
+                uri += '&' + param
+            else:
+                uri += '?' + param
+        return gdata.service.GDataService.Get(self, uri, converter=converter)
+
     def GetTagFeed(self, uri):
         return self.Get(uri, converter=douban.TagFeedFromString)
+
+
+class Query(gdata.service.Query):
+    def __init__(self, feed=None, text_query=None, start_index=None,
+            max_results=None, **params):
+        gdata.service.Query.__init__(self, feed=feed, text_query=text_query,
+                params=params)
+        if start_index is not None:
+            self.start_index = start_index
+        if max_results is not None:
+            self.max_results = max_results
--- tests/test_service.py
+++ tests/test_service.py
@@ -9,3 +9,16 @@ class TestDoubanService:
     def test_people(self):
         people = self.client.GetPeople('http://api.douban.com/people/1000001')
         assert people.title.text == "阿北"
+
+    def test_search_people(self):
+        feed = self.client.SearchPeople("阿北")
+        found = False
+        for entry in feed.entry:
+            if entry.title.text == "阿北":
+                found = True
+                break
+        assert found
+
+    def test_query_book_by_tag(self):
+        feed = self.client.QueryBookByTag("小说")
+        assert len(feed.entry)
