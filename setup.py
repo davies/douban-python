eeb1e048532e9f37ccdd7ebf5d572081a5646bb6
hongqn <hongqn@bc624eda-5042-0410-a9c9-c735abdd7021>
2009-05-16 15:41:32 +0000

    version bump to 0.3.0

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
         name = 'douban-python',
-        version = '0.1.1',
+        version = '0.3.0',
         description = "Python client library for Douban APIs",
         license = "Apache 2.0",
         author = "Qiangning Hong",
         author_email = "hongqn@gmail.com",
         url = "http://www.douban.com/service/apidoc/",
-        install_requires = ['setuptools', 'gdata.py>=1.1.1'],
+        install_requires = ['setuptools', 'gdata>=1.1.1'],
         tests_require = ['nose'],
         packages = find_packages(),
         #test_suite = 'nose.collector',
