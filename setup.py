885d6c68fba4f1d2b43de7ac51b1a168727a815e
hongqn <hongqn@bc624eda-5042-0410-a9c9-c735abdd7021>
2010-01-17 15:00:00 +0000

    correct project url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
         license = "Apache 2.0",
         author = "Qiangning Hong",
         author_email = "hongqn@gmail.com",
-        url = "http://www.douban.com/service/apidoc/",
+        url = "http://code.google.com/p/douban-python/",
         install_requires = ['setuptools', 'gdata>=1.1.1'],
         tests_require = ['nose'],
         packages = find_packages(),
